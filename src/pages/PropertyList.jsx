143a39434b10e317f6191688cc2d78829b7e96c9
Thibaut <thibautlegoff4@gmail.com>
2026-02-27 13:27:39 +0100

    final commit

--- src/pages/PropertyList.jsx
+++ src/pages/PropertyList.jsx
@@ -4,14 +4,19 @@ import logements from '../data/logements.json'
 
 function PropertyList() {
   return (
+    // Conteneur de toutes les cartes logements
     <div className="property-list">
+      {/* On parcourt la liste des logements */}
       {logements.map((logement) => (
+        // Chaque carte est un lien vers la page de détail
         <Link
           key={logement.id}
           className="property-card"
           to={`/property/${logement.id}`}
         >
+          {/* Image principale du logement */}
           <img src={logement.cover} alt={logement.title} />
+          {/* Titre du logement */}
           <h3>{logement.title}</h3>
         </Link>
       ))}
